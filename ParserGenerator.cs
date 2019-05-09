7886af7b68815e491d897411e99975f045158a76
HyunJun Jeong <rollrat@naver.com>
2019-05-10 00:48:40 +0900

    Update ParserGenerator.cs

--- ParserGenerator.cs
+++ ParserGenerator.cs
@@ -590,7 +590,7 @@ namespace ParserGenerator
             }
             return states;
         }
-
+       
         /// <summary>
         /// Get FIRST items with lookahead (Build specific states completely)
         /// </summary>
@@ -627,6 +627,46 @@ namespace ParserGenerator
                     }
                 }
             }
+
+            // (production_rule_index + sub_productions_pos + dot_position), (states_index)
+            var states_prefix2 = new Dictionary<string, int>();
+            var states_count = 0;
+            bool change = false;
+
+            do
+            {
+                change = false;
+                q.Enqueue(lookahead_with_first(production_rule_index, sub_production, sub_production_index, pred));
+                while (q.Count != 0)
+                {
+                    var ll = q.Dequeue();
+                    foreach (var e in ll)
+                    {
+                        var ii = i2s(e.Item1, e.Item2, e.Item3);
+                        if (!states_prefix2.ContainsKey(ii))
+                        {
+                            states_prefix2.Add(ii, states_count);
+                            foreach (var hse in e.Item4)
+                                if (!states[states_prefix[ii]].Item4.Contains(hse))
+                                {
+                                    change = true;
+                                    states[states_prefix[ii]].Item4.Add(hse);
+                                }
+                            q.Enqueue(lookahead_with_first(e.Item1, e.Item2, e.Item3, states[states_count].Item4));
+                            states_count++;
+                        }
+                        else
+                        {
+                            foreach (var hse in e.Item4)
+                                if (!states[states_prefix[ii]].Item4.Contains(hse))
+                                {
+                                    change = true;
+                                    states[states_prefix[ii]].Item4.Add(hse);
+                                }
+                        }
+                    }
+                }
+            } while (change);
 
             return states;
         }
